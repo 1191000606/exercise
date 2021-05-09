c5ac35e98805545c932fdd35c430afecd317a3f7
Chen Yifan <1191000606@stu.hit.edu.cn>
2021-05-09 15:42:04 +0800

    separate input with calculation

--- src/exercise_6_6/HIndex.java
+++ src/exercise_6_6/HIndex.java
@@ -7,46 +7,54 @@ import java.util.List;
 import java.util.Scanner;
 
 public class HIndex {
-    public static int hIndex(List<Integer> citations) {
-        int hindex = 0;
-        for (int j = 0; j < citations.size(); j++) {
-            if (citations.get(j) >= j + 1)
-                hindex = j + 1;
-            else
-                break;
-        }
-
-        return hindex;
-    }
-
-    public static void main(String[] args) {
-        File file = new File("./src/exercise_6_6/data.txt");
+    private static List<String> getLines(String filename) {
+        File file = new File("./src/exercise_6_6/" + filename);
         BufferedReader reader;
 
         try {
             reader = new BufferedReader(new FileReader(file));
         } catch (FileNotFoundException ignored) {
             System.out.println("File is not found");
-            return;
+            return null;
         }
 
+        List<String> lines = new ArrayList<>();
         String line;
+        int num = 1;
 
-        try {
-            line = reader.readLine();
-        } catch (IOException ioException) {
-            System.out.println("Input error occurs");
-            return;
+        while (true) {
+            try {
+                line = reader.readLine();
+            } catch (IOException ioException) {
+                System.out.println("Fail to read Line " + num);
+                num++;
+                continue;
+            }
+
+            if (line == null) {
+                if (num == 1) {
+                    System.out.println("File is empty");
+                }
+                break;
+            }
+
+            if (line.length() == 0) {
+                System.out.println("Line " + num + " is empty, ignore this line");
+                num++;
+                continue;
+            }
+
+            num++;
+            lines.add(line);
         }
 
-        if (line == null || line.length() == 0) {
-            System.out.println("The first line of file is empty");
-            return;
-        }
+        return lines;
+    }
 
-        String[] num = line.split(",");
+    private static int delLine(String line) {
         List<Integer> citations = new ArrayList<>();
 
+        String[] num = line.split(",");
 
         for (int i = 0; i < num.length; i++) {
             if (!num[i].matches("[0-9]+")) {
@@ -64,9 +72,39 @@ public class HIndex {
         citations.sort(Integer::compareTo);
         Collections.reverse(citations);
 
-        int hindex = hIndex(citations);
+        return calHIndex(citations);
+    }
 
-        System.out.println("The h-index is: " + hindex);
+    private static int calHIndex(List<Integer> citations) {
+        int hindex = 0;
+        for (int j = 0; j < citations.size(); j++) {
+            if (citations.get(j) >= j + 1)
+                hindex = j + 1;
+            else
+                break;
+        }
+
+        return hindex;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Please input the name of file which exists in Package exercise_6_6");
+
+        String filename;
+        Scanner scanner = new Scanner(System.in);
+        filename = scanner.nextLine();
+
+        List<String> lines = getLines(filename);
+
+        if (lines == null) {
+            return;
+        }
+
+        int index = 1;
+        for (String line : lines) {
+            System.out.println("Line " + index + ": h-index is " + delLine(line));
+            index++;
+        }
     }
 
 }
